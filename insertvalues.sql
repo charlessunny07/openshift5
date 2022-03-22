8fc27c3fe4c39dd0d045237c29d40c3f55991fab
Sunny <charles.sunny@cgi.com>
2022-03-22 12:16:31 -0600

    1

---
+++ insertvalues.sql
@@ -0,0 +1,4 @@
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('PETER',1)
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('JOSEPH',2)
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('MARK',3)
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('MIKE',4)
