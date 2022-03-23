e90f47f0c2356d1f28363bb52b54c56ecdf338e6
charlessunny07 <101823830+charlessunny07@users.noreply.github.com>
2022-03-22 20:49:50 -0600

    Update insertvalues.sql

--- insertvalues.sql
+++ insertvalues.sql
@@ -2,3 +2,4 @@ INSERT INTO priarworld (firstname,rollnumber) VALUES ('PETER',1);
 INSERT INTO priarworld (firstname,rollnumber) VALUES ('JOSEPH',2);
 INSERT INTO priarworld (firstname,rollnumber) VALUES ('MARK',3);
 INSERT INTO priarworld (firstname,rollnumber) VALUES ('MIKE',4);
+INSERT INTO priarworld (firstname,rollnumber) VALUES ('CHARLES',5);
